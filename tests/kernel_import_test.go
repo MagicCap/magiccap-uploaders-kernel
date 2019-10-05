0a21cf55fc973d027c1dd5eb80ae47c35df280ee
Jake Gealer <jake@gealer.email>
2019-10-05 19:38:40 +0100

    Move tests

--- tests/kernel_import_test.go
+++ tests/kernel_import_test.go
@@ -1,17 +1,18 @@
-package MagicCapKernel
+package tests
 
 import (
 	"encoding/json"
 	"io/ioutil"
+	"magiccap-uploaders-kernel"
 	"magiccap-uploaders-kernel/standards"
 	"testing"
 )
 
 func TestV1Import(t *testing.T) {
-	k := Kernel{
+	k := MagicCapKernel.Kernel{
 		Uploaders: map[string]*MagicCapKernelStandards.Uploader{},
 	}
-	f, err := ioutil.ReadFile("./routes/v1.json")
+	f, err := ioutil.ReadFile("../routes/v1.json")
 	if err != nil {
 		t.Error("Failed to open V1 routes. Does it exist?")
 	}
