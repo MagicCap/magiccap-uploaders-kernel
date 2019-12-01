c04b9d4e8d0c3f0c6faca47c1bbff5690b9d89cb
Jake Gealer <jake@gealer.email>
2019-12-01 16:15:04 +0000

    Allow uploaders to be serialized

--- standards/standards.go
+++ standards/standards.go
@@ -10,27 +10,27 @@ type ConfigOption struct {
 
 // Uploader defines the uploader in its final form.
 type Uploader struct {
-	Icon          string
-	Description   string
-	Name          string
-	ConfigOptions map[string]ConfigOption
-	Upload        func(Config map[string]interface{}, Data []byte, Filename string) (string, error)
+	Icon          string                                                                            `json:"icon"`
+	Description   string                                                                            `json:"description"`
+	Name          string                                                                            `json:"name"`
+	ConfigOptions map[string]ConfigOption                                                           `json:"configOptions"`
+	Upload        func(Config map[string]interface{}, Data []byte, Filename string) (string, error) `json:"-"`
 }
 
 // UploaderStructure defines the structure that an imported uploaders JSON will follow.
 type UploaderStructure struct {
-	Icon           string                  `json:"icon"`
-	Name           string                  `json:"name"`
-	Description    string                  `json:"description"`
-	Config         map[string]ConfigOption `json:"config"`
-	Spec           interface{}             `json:"spec"`
+	Icon        string                  `json:"icon"`
+	Name        string                  `json:"name"`
+	Description string                  `json:"description"`
+	Config      map[string]ConfigOption `json:"config"`
+	Spec        interface{}             `json:"spec"`
 }
 
 // GetStandardsMap gets the standards map.
 func GetStandardsMap() map[string]func(Structure UploaderStructure) (*Uploader, error) {
 	return map[string]func(Structure UploaderStructure) (*Uploader, error){
 		"http": HTTPInit,
-		"ftp": FTPInit,
-		"s3": S3Init,
+		"ftp":  FTPInit,
+		"s3":   S3Init,
 	}
 }
